61dece8480bdfda2432ad93078307dddfeaec884
Zahid Mahir <zahid@zmhr.me>
2014-02-08 15:01:57 -0500

    removed cake, readded

--- db/relate.sql
+++ db/relate.sql
@@ -1,63 +1,188 @@
-CREATE TABLE `users` (
-`id` INTEGER NOT NULL AUTO_INCREMENT ,
-`health` INTEGER DEFAULT NULL ,
-`email` VARCHAR(255) DEFAULT NULL ,
-`password` VARCHAR(255) DEFAULT NULL ,
-`created` DATETIME DEFAULT NULL ,
-`modified` DATETIME DEFAULT NULL ,
-PRIMARY KEY (`id`)
-);
+-- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: relate
+-- ------------------------------------------------------
+-- Server version	5.5.35-0ubuntu0.12.04.2-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activities`
+--
+
+DROP TABLE IF EXISTS `activities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `activities` (
-`id` INTEGER NOT NULL AUTO_INCREMENT ,
-`name` VARCHAR(255) DEFAULT NULL ,
-`frequency` INTEGER DEFAULT NULL ,
-`created` DATETIME DEFAULT NULL ,
-`modified` DATETIME DEFAULT NULL ,
-PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `frequency` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `activities`
+--
+
+LOCK TABLES `activities` WRITE;
+/*!40000 ALTER TABLE `activities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `activities_users`
+--
+
+DROP TABLE IF EXISTS `activities_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `activities_users` (
-`activity_id` INTEGER DEFAULT NULL ,
-`user_id` INTEGER DEFAULT NULL ,
-UNIQUE KEY (`activity_id`, `user_id`)
-);
+  `activity_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  UNIQUE KEY `activity_id` (`activity_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `activities_users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `activities_users_ibfk_1` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `questions` (
-`id` INTEGER NOT NULL AUTO_INCREMENT ,
-`content` VARCHAR(255) DEFAULT NULL ,
-`type` VARCHAR(255) DEFAULT NULL ,
-`created` DATETIME DEFAULT NULL ,
-`modified` DATETIME DEFAULT NULL ,
-PRIMARY KEY (`id`)
-);
+--
+-- Dumping data for table `activities_users`
+--
 
+LOCK TABLES `activities_users` WRITE;
+/*!40000 ALTER TABLE `activities_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activities_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `choices`
+--
+
+DROP TABLE IF EXISTS `choices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `choices` (
-`id` INTEGER NOT NULL AUTO_INCREMENT ,
-`value` INTEGER DEFAULT NULL ,
-`content` VARCHAR(255) DEFAULT NULL ,
-`created` DATETIME DEFAULT NULL ,
-`modified` DATETIME DEFAULT NULL ,
-`question_id` INTEGER DEFAULT NULL ,
-PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` int(11) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `choices`
+--
+
+LOCK TABLES `choices` WRITE;
+/*!40000 ALTER TABLE `choices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `choices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `responses`
+--
+
+DROP TABLE IF EXISTS `responses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `responses` (
-`id` INTEGER NOT NULL AUTO_INCREMENT ,
-`value` INTEGER DEFAULT NULL ,
-`created` DATETIME DEFAULT NULL ,
-`modified` DATETIME DEFAULT NULL ,
-`activity_id` INTEGER DEFAULT NULL ,
-`user_id` INTEGER DEFAULT NULL ,
-`choice_id` INTEGER DEFAULT NULL ,
-`question_id` INTEGER DEFAULT NULL ,
-PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `activity_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `choice_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE `users` ADD FOREIGN KEY (id) REFERENCES `responses` (`user_id`);
-ALTER TABLE `activities` ADD FOREIGN KEY (id) REFERENCES `responses` (`activity_id`);
-ALTER TABLE `activities_users` ADD FOREIGN KEY (activity_id) REFERENCES `activities` (`id`);
-ALTER TABLE `activities_users` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
-ALTER TABLE `questions` ADD FOREIGN KEY (id) REFERENCES `choices` (`question_id`);
-ALTER TABLE `questions` ADD FOREIGN KEY (id) REFERENCES `responses` (`question_id`);
-ALTER TABLE `choices` ADD FOREIGN KEY (id) REFERENCES `responses` (`choice_id`);
+--
+-- Dumping data for table `responses`
+--
+
+LOCK TABLES `responses` WRITE;
+/*!40000 ALTER TABLE `responses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `responses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `health` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-02-08 19:51:22
